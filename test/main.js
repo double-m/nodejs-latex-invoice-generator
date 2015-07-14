3228cd70002bcddd461540cd884a8472c86a455f
Marcello Messori <marcello.mm.messori@gmail.com>
2015-07-14 16:06:09 +0200

    added fancy date formatting

--- test/main.js
+++ test/main.js
@@ -12,16 +12,18 @@ describe('Test the code generation', function() {
         line3: { myVar: 10000.00 }
       };
 
-      expect(jsontotex(obj.line1)).to.equal('\\def \\myVar {' + obj.line1.myVar + '}');
-      expect(jsontotex(obj.line2)).to.equal('\\def \\myVar {' + obj.line2.myVar + '}');
-      expect(jsontotex(obj.line3)).to.equal('\\def \\myVar {' + obj.line3.myVar + '}');
+      expect(jsontotex.formatPair(obj.line1)).to.equal('\\def \\myVar {' + obj.line1.myVar + '}');
+      expect(jsontotex.formatPair(obj.line2)).to.equal('\\def \\myVar {' + obj.line2.myVar + '}');
+      expect(jsontotex.formatPair(obj.line3)).to.equal('\\def \\myVar {' + obj.line3.myVar + '}');
     });
 
-    it.skip('date from YYYY-MM-DD to locale (EN and IT). How to get the locale?');
+    it('date from YYYY-MM-DD to IT locale (EN is the default)', function() {
+      expect(jsontotex.formatVal('InvoiceIsoDate', '2015-07-01')).to.equal('Mercoledì 01 luglio 2015');
+      //expect(jsontotex.formatVal('InvoiceIsoDate', '2015-07-01')).to.equal('Wednesday, July 01, 2015');
+    });
 
     it.skip('parse JSON file');
 
   });
 
 });
-
